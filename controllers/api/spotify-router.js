6f702fd99e0f9ac026fd2d644533cd3d38c463af
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-03-29 22:03:54 -0700

    Fixed the spotify router to get token

--- controllers/api/spotify-router.js
+++ controllers/api/spotify-router.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const express = require('express');
 const app = express();
-const spotAuth = require("../../util/spotAuth");
+const spotAuth = require("../../util/spotifyAuth");
 
 app.get('/spotify-auth', (req, res) => {
     spotAuth(res);
 });
+
+spotAuth();
+
+// 
