71e0a7b27088fb172704a079fd5e7d63d7b9218a
twqueen <tiffanyytsai@gmail.com>
2023-04-01 15:52:36 -0700

    little edits to router and handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const usersRouter = require('./users-router');
 const commentRouter = require('./comment-router');
 const postRouter = require('./post-router');
+const dashboardRouter = require('./dashboard');
 
 router.use('/users', usersRouter);
 router.use('/comment', commentRouter);
 router.use('/post', postRouter);
+router.user('/profile', dashboardRouter);
 
 module.exports = router;
