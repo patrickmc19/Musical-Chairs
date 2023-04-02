7fb3f07941d97703e2113ab1a1c4b3b07eda9066
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-04-02 12:14:59 -0700

    imported spotAuth in the router

--- controllers/api/spotify-router.js
+++ controllers/api/spotify-router.js
@@ -1,18 +1,25 @@
 const router = require('express').Router();
 const express = require('express');
 const app = express();
+const spotAuth = require("../../util/spotifyAuth");
+
 // const spotAuth = require("../../util/spotifyAuth");
 const axios = require('axios');
-const accessToken = 'BQDtEzHM5alRks0G79mIW5zn_rW1RN-EuAg0JCjYa4xaYjT_P1FlXXWGdReMG1OWT9GmhxF_jMr8Qp7Xo2jEibGJDKJvASmUlSXDsLLUwAy85veXOmx5 ';
+const accessToken = 'BQBgyyqT4H1H1-Wu5DwPKquqUA4fOxNikEE90DLw7z_XGZb5d2Slq3jeiKB7bDK6o7uzUtYqbOXvXBeYRD_aEcPPVMod3yL-ZdrEeEeiK1h4UuldQ4Uu';
 
-// app.get('/spotify-auth', (req, res) => {
-//     spotAuth(res);
-// });
+app.get('/spotify-auth', (req, res) => {
+    spotAuth(res);
+});
 
-app.get('/tracks', async (req, res) => {
+spotAuth();
+
+
+app.get('/search', async (req, res) => {
+    console.log('search request received');
     const query = req.query.q;
     const type = 'track';
     const url = `https://api.spotify.com/v1/search?q=${query}&type=${type}`;
+    console.log('url:', url);
 
     try {
         const response = await axios.get(url, {
@@ -27,6 +34,8 @@ app.get('/tracks', async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+
 
 // spotAuth();
 module.exports = router
