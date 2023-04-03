19bb5bb56a33f87e39790a1abea51e5584fd1d96
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-04-02 19:35:00 -0700

    fixed issues with user model and the routes

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -14,7 +14,7 @@ router.get('/', withAuth, async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
@@ -86,7 +86,7 @@ router.post('/:id/comments', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
@@ -122,7 +122,7 @@ router.put('/post/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/profile/:id', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
