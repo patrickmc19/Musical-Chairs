b8e373e6be013d96edd8622d5759501b948dee29
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-04-05 20:28:38 -0700

    made it so that the profiles are no longer links

--- public/js/home.js
+++
@@ -1,6 +0,0 @@
-// app.get('/search', function(req, res) {
-//     var query = req.query.query;
-//     var results = performCommentSearch(query); // Perform search and get results
-    
-//     res.render('search-results', { query: query, results: results });
-//   });
